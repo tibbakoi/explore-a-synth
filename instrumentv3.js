ddee196c99997fa936534dbae7e262991f175e9b
KatYoung <kat.young@york.ac.uk>
2020-11-03 14:33:35 +0000

    removed text placeholder from html. added some comments. refactored drawKeyboardIndicator

--- instrumentv3.js
+++ instrumentv3.js
@@ -14,7 +14,7 @@ let toggle_Type1, toggle_Type2, toggle_Type3, toggle_Type4 //osc type
 let toggle_record; //record
 let XY_freqAmp; //x-y control
 
-//loudspeaker graphic
+//loudspeaker graphic - needs to be defined so that draws when no sound playing
 let currentWidth = 100;
 
 //music stuff
@@ -25,6 +25,9 @@ let recordState = 0; //1 if recording
 let saveState = 0; // 1 is file exists to save
 let recorder, soundFile;
 let button_Playback, button_Save;
+
+//scene manager buttons
+let button_loudspeakerMore;
 
 function setup() {
     let canvas = createCanvas(colWidth * 3 + spacingOuter * 4, rowHeight * 2 + spacingOuter * 3);
@@ -41,20 +44,27 @@ function setup() {
     //UI objects using touchGUI library
     gui = createGui();
 
+    // sound settings toggles
     toggle_OnOff = createCheckbox("OnOff", spacingOuter + spacingInner, spacingOuter + spacingInner, buttonHeight, buttonHeight);
     toggle_Type1 = createCheckbox("Sine", spacingOuter + spacingInner, spacingOuter + spacingInner * 2 + buttonHeight, buttonHeight, buttonHeight, 1)
     toggle_Type2 = createCheckbox("Saw", spacingOuter + spacingInner * 2 + buttonHeight, spacingOuter + spacingInner * 2 + buttonHeight, buttonHeight, buttonHeight, 0);
     toggle_Type3 = createCheckbox("Tri", spacingOuter + spacingInner * 3 + buttonHeight * 2, spacingOuter + spacingInner * 2 + buttonHeight, buttonHeight, buttonHeight, 0);
     toggle_Type4 = createCheckbox("Squ", spacingOuter + spacingInner * 4 + buttonHeight * 3, spacingOuter + spacingInner * 2 + buttonHeight, buttonHeight, buttonHeight, 0);
 
+    //X-Y pad
     XY_freqAmp = createSlider2d("freqAmp", colWidth + spacingOuter * 2, spacingOuter, colWidth, rowHeight, 0, 1, 1, 127);
 
+    //keyboard toggles
     toggle_controlType = createCheckbox("control", spacingOuter * 2 + colWidth + spacingInner, spacingOuter * 2 + rowHeight + spacingInner, buttonHeight, buttonHeight)
     toggle_sustain = createCheckbox("sustain", spacingOuter * 2 + colWidth * 2 - spacingInner - buttonHeight, spacingOuter * 2 + rowHeight + spacingInner, buttonHeight, buttonHeight)
 
+    //record UI
     toggle_record = createCheckbox("recording", spacingOuter * 3 + colWidth * 2.5 + spacingInner * 2, height - buttonHeight * 3 - spacingOuter - spacingInner * 3, buttonHeight, buttonHeight)
     button_Playback = createButton("Play", spacingOuter * 3 + colWidth * 2.5 + spacingInner * 2, height - buttonHeight * 2 - spacingOuter - spacingInner * 2, colWidth / 2 - spacingOuter - spacingInner, buttonHeight)
     button_Save = createButton("Save", spacingOuter * 3 + colWidth * 2.5 + spacingInner * 2, height - buttonHeight - spacingOuter - spacingInner, colWidth / 2 - spacingOuter - spacingInner, buttonHeight)
+
+    // scene manager switch buttons
+    button_loudspeakerMore = createButton("?", spacingOuter + colWidth * 2.5 - spacingInner * 3, height - spacingOuter - spacingInner - 25, 25, 25)
 
     drawRectangles();
 
@@ -76,13 +86,14 @@ function setup() {
 function draw() {
     background(100);
     //placeholders
-    text('Filtering and FX...?!', spacingOuter + spacingInner, spacingOuter + rowHeight * 1.5)
 
     drawRectangles();
-    drawLoudspeaker((spacingOuter * 3) + (colWidth * 2.25), (spacingOuter * 2) + (rowHeight * 1.5));
+    drawLoudspeaker();
+    drawRecordLED();
 
     drawGui();
 
+    //various text bits
     fill("white");
     textSize(25);
     textAlign(LEFT, CENTER);
@@ -91,12 +102,13 @@ function draw() {
     textAlign(CENTER, CENTER);
     text('Record', spacingOuter * 3 + spacingInner * 2 + colWidth * 2.75, spacingOuter * 2 + spacingInner + rowHeight + 15);
     textAlign(LEFT, CENTER);
-
+    text('Filtering and FX...?!', spacingOuter + spacingInner, spacingOuter + rowHeight * 1.5)
     noFill();
 
-    drawRecordLED();
+    //get and draw waveform
+    drawWaveform(fft.waveform());
 
-    //----- UI interactions -----//
+    //----- define UI interactions -----//
 
     // turn synth on/off
     if (toggle_OnOff.val) {
@@ -133,24 +145,29 @@ function draw() {
     //display osc type label
     if (toggle_Type1.val) {
         fill("white");
+        noStroke();
         text('Sine', spacingOuter + spacingInner * 5 + buttonHeight * 4, spacingOuter + spacingInner + buttonHeight + 25);
     } else if (toggle_Type2.val) {
         fill("white");
+        noStroke();
         text('Saw', spacingOuter + spacingInner * 5 + buttonHeight * 4, spacingOuter + spacingInner + buttonHeight + 25);
     } else if (toggle_Type3.val) {
         fill("white");
+        noStroke();
         text('Tri', spacingOuter + spacingInner * 5 + buttonHeight * 4, spacingOuter + spacingInner + buttonHeight + 25);
     } else if (toggle_Type4.val) {
         fill("white");
+        noStroke();
         text('Sqr', spacingOuter + spacingInner * 5 + buttonHeight * 4, spacingOuter + spacingInner + buttonHeight + 25);
     }
 
     //frequency/amplitude control
     if (XY_freqAmp.isChanged) {
-        oscillator.amp(XY_freqAmp.valX);
+        oscillator.amp(XY_freqAmp.valX, 0.01);
         oscillator.freq(midiToFreq(XY_freqAmp.valY));
     }
 
+    // draw active keyboard
     if (toggle_controlType.val) {
         drawKeyboard(1); //active keyboard
     } else {
@@ -170,67 +187,67 @@ function draw() {
                     case "a": //C
                         currentNote = 0;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 1, height - spacingOuter - 30);
+                        drawKeyboardIndicator(currentNote);
                         break;
                     case "w": //C#
                         currentNote = 1;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 2, height - spacingOuter - 70);
+                        drawKeyboardIndicator(currentNote);
                         break;
                     case "s": //D
                         currentNote = 2;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 3, height - spacingOuter - 30);
+                        drawKeyboardIndicator(currentNote);
                         break;
                     case "e": //D#
                         currentNote = 3;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 4, height - spacingOuter - 70);
+                        drawKeyboardIndicator(currentNote);
                         break;
                     case "d": //E
                         currentNote = 4;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 5, height - spacingOuter - 30);
+                        drawKeyboardIndicator(currentNote);
                         break;
                     case "f": //F
                         currentNote = 5;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 7, height - spacingOuter - 30);
+                        drawKeyboardIndicator(currentNote);
                         break;
                     case "t": //F#
                         currentNote = 6;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 8, height - spacingOuter - 70);
+                        drawKeyboardIndicator(currentNote);
                         break;
                     case "g": //G
                         currentNote = 7;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 9, height - spacingOuter - 30);
+                        drawKeyboardIndicator(currentNote);
                         break;
                     case "y": //G#
                         currentNote = 8;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 10, height - spacingOuter - 70);
+                        drawKeyboardIndicator(currentNote);
                         break;
                     case "h": //A
                         currentNote = 9;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 11, height - spacingOuter - 30);
+                        drawKeyboardIndicator(currentNote);
                         break;
                     case "u": //A#
                         currentNote = 10;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 12, height - spacingOuter - 70);
+                        drawKeyboardIndicator(currentNote);
                         break;
                     case "j": //B
                         currentNote = 11;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 13, height - spacingOuter - 30);
+                        drawKeyboardIndicator(currentNote);
                         break;
                     case "k": //C
                         currentNote = 12;
                         playNote();
-                        drawKeyboardIndicator(spacingOuter * 2 + colWidth + spacingInner + 18 * 15, height - spacingOuter - 30);
+                        drawKeyboardIndicator(currentNote);
                         break;
                 }
             }
@@ -245,10 +262,12 @@ function draw() {
             recorder.stop();
         }
     }
+
     // play recorded file, if exists
     if (button_Playback.isPressed && soundFile.duration() > 0) { //make sure sound file exists...
         soundFile.play();
     }
+
     //turn play button green when sound file is playing
     if (soundFile.isPlaying()) {
         button_Playback.setStyle({
@@ -260,33 +279,14 @@ function draw() {
             fillBg: color(130),
         });
     }
+
     // save recorded file, if exists
     if (button_Save.isPressed && soundFile.duration() > 0) { //make sure sound file exists...
         save(soundFile, 'MySoundFile.wav');
     }
-
-    //get waveform
-    let waveform = fft.waveform();
-    // draw waveform
-    drawWaveform(waveform);
-
 }
 
-function drawKeyboardIndicator(x, y) {
-    fill("red");
-    circle(x, y, 10);
-    noFill();
-}
-
-function drawOctaveIndicator(currentOctave) {
-    let activeOctave = currentOctave / 12 - 1;
-    console.log(activeOctave)
-    stroke("red");
-    noFill();
-
-    circle(spacingOuter * 2 + colWidth + spacingInner + ((activeOctave * 2) - 1) * 18, height - spacingOuter - 50 * 2 - 18, 15);
-
-}
+//----- musical functions -----//
 
 function playNote() {
     XY_freqAmp.valY = currentNote + currentOctave;
@@ -340,8 +340,10 @@ function drawWaveform(waveform) {
     endShape();
 }
 
-function drawLoudspeaker(x, y) {
+function drawLoudspeaker() {
     //centre of loudspeaker = x, y
+    x = (spacingOuter * 3) + (colWidth * 2.25);;
+    y = (spacingOuter * 2) + (rowHeight * 1.5);
 
     //change the value every 5 frames, if the synth is turned on
     if (frameCount % 3 == true && toggle_OnOff.val) {
@@ -402,6 +404,44 @@ function drawKeyboard(activeFlag) {
     noFill();
 }
 
+function drawKeyboardIndicator(currentNote) {
+    let whiteNoteOffsetVertical = 30;
+    let blackNoteOffsetVertical = 70;
+    let horizIncrement, verticalOffset;
+
+    //calculate multiple of horiz increment for each note
+    if (currentNote < 5) {
+        horizIncrement = 1;
+    } else if (currentNote > 4 && currentNote < 12) {
+        horizIncrement = 2;
+    } else {
+        horizIncrement = 3;
+    }
+
+    //calculate y offset for each note
+    if (currentNote == 1 || currentNote == 3 || currentNote == 6 || currentNote == 8 || currentNote == 10) {
+        verticalOffset = blackNoteOffsetVertical;
+    } else {
+        verticalOffset = whiteNoteOffsetVertical;
+    }
+
+    x = spacingOuter * 2 + colWidth + spacingInner + 18 * (currentNote + horizIncrement); //18 = width of key/2
+    y = height - spacingOuter - verticalOffset;
+
+    fill("red");
+    circle(x, y, 10);
+    noFill();
+}
+
+function drawOctaveIndicator(currentOctave) {
+    let activeOctave = currentOctave / 12 - 1;
+    console.log(activeOctave)
+    stroke("red");
+    noFill();
+
+    circle(spacingOuter * 2 + colWidth + spacingInner + ((activeOctave * 2) - 1) * 18, height - spacingOuter - 50 * 2 - 18, 15);
+
+}
 // to avoid autoplay
 function touchStarted() {
     if (getAudioContext().state !== 'running') {
